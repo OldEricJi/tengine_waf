ba9bc37929160b118883044f7731421af4efa84e
OldEricJi <70852719+OldEricJi@users.noreply.github.com>
2025-03-01 02:05:26 +0800

    Update waf.lua

--- conf/waf.lua
+++ conf/waf.lua
@@ -1,19 +1,70 @@
 local content_length=tonumber(ngx.req.get_headers()['content-length'])
 local method=ngx.req.get_method()
-local ngxmatch=ngx.re.match
+local ngxmatch=ngx.re.find
+
+
+local geoip = ngx.var.geoip_remote_addr
+
+if ngx.var.whiteip == "1" then
+    log('whiteip check', geoip)
+    return
+end
+
+function getClientIp()
+    -- 如果通过 cloudflare，调用其传递来的信息
+    local cf_connecting_ip = ngx.req.get_headers()["CF-Connecting-IP"]
+
+    if cf_connecting_ip then
+        return cf_connecting_ip
+    end
+
+    local x_forwarded_for = ngx.req.get_headers()["X-Forwarded-For"]
+
+    if x_forwarded_for then
+        local ips = {}
+        for ip in string.gmatch(x_forwarded_for, "[^,]+") do
+            table.insert(ips, ip:trim())
+        end
+        -- 获取第一个IP，也就是客户端的原始IP
+        if #ips > 0 then
+            return ips[1]
+        end
+    end
+
+    -- 如果没有CF-Connecting-IP和X-Forwarded-For，则使用 remote_addr
+    local remote_addr = ngx.var.remote_addr
+
+    if remote_addr then
+        return remote_addr
+    end
+
+    return "unknown"
+end
+
+
+ngx.var.remote_addr = getClientIp()
+
 
 if whiteip() then
+    return
+elseif whiteurl() then
+    return
 elseif blockip() then
+    ngx.exit(444)
 elseif denycc() then
+    ngx.exit(444)
 elseif ngx.var.http_Acunetix_Aspect then
     ngx.exit(444)
 elseif ngx.var.http_X_Scan_Memo then
     ngx.exit(444)
-elseif whiteurl() then
 elseif ua() then
+    ngx.exit(444)
 elseif url() then
+    ngx.exit(444)
 elseif args() then
+    ngx.exit(444)
 elseif cookie() then
+    ngx.exit(444)
 elseif PostCheck then
     if method=="POST" then   
             local boundary = get_boundary()
